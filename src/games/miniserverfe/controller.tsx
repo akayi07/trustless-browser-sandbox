69ea6044a2076e7a1542cec9b22dd2285137d91f
akayi <zhuolimiaotx@163.com>
2024-08-13 12:46:44 +0800

    add nonce

--- src/games/miniserverfe/controller.tsx
+++ src/games/miniserverfe/controller.tsx
@@ -46,7 +46,7 @@ export function GameController() {
   const [playerIds, setPlayerIds] = useState("");
   const [localValues, setLocalValues] = useState<number[]>([]);
   const [objects, setObjects] = useState<Array<ObjectProperty>>([]);
-
+  const [nonce, setNonce] = useState(0n);
 
   // modified modifier array
   const [dropList, setDropList] = useState<Array<number|null>>([null, null, null, null, null, null, null, null]);
@@ -136,7 +136,7 @@ export function GameController() {
       const playerIdHex = "0x" + BigInt(playerId).toString(16);
       setPlayerIds(playerIdHex);
 
-      const insPlayerCmd = createCommand(CMD_INSTALL_PLAYER, 0n);
+      const insPlayerCmd = createCommand(0n, CMD_INSTALL_PLAYER, 0n);
       dispatch(sendTransaction({cmd: [insPlayerCmd, 0n, 0n, 0n], prikey: l2account!.address}));
     } catch(e) {
       dispatch(setErrorMessage("Error at create player " + e));
@@ -161,6 +161,9 @@ export function GameController() {
 
       const data = JSON.parse(res.data);
       console.log("query state data", data);
+
+      setNonce(BigInt(data[0].nonce));
+
       if (playerAction == "creating") {
         decodePlayerInfo(data[0]);
         dispatch(setGlobalTimer(data[2]));
@@ -298,7 +301,7 @@ export function GameController() {
               {<CreateButton objects={objects} />}
               </div>
             </div>
-            <Explore objects={objects} modifiers={dropList}/>
+            <Explore objects={objects} modifiers={dropList} nonce={nonce}/>
             <div className="program">
               <div className="title">PROGRAM</div>
               <div className="draggableBox">
